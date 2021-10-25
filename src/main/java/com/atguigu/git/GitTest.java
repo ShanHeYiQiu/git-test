aad481cde67714c9559c4bac73023bee1563c682
ShanHeYiQiu <91532483+ShanHeYiQiu@users.noreply.github.com>
2021-10-25 18:59:12 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("git push");
+         System.out.println("git pull");
     }
 }
